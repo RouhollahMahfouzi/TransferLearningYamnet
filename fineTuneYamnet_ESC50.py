26a8e64615fdbf9c0d939024619f69a72269add1
salar <rouhollah.mahfouzi@gmail.com>
2024-07-19 15:41:41 +0200

    model is trained successfully for classifying cat and dog sounds using ECM-50 dataset

--- fineTuneYamnet_ESC50.py
+++ fineTuneYamnet_ESC50.py
@@ -24,15 +24,25 @@ def load_wav_16k_mono(filename):
     wav = tfio.audio.resample(wav, rate_in=sample_rate, rate_out=16000)
     return wav
 
+
 def load_wav_for_map(filename, label, fold):
   return load_wav_16k_mono(filename), label, fold
 
 yamnet_model_handle = 'https://tfhub.dev/google/yamnet/1'
 yamnet_model = hub.load(yamnet_model_handle)
 
+testing_wav_file_name = tf.keras.utils.get_file('miaow_16k.wav',
+                                                'https://storage.googleapis.com/audioset/miaow_16k.wav',
+                                                cache_dir='./',
+                                                cache_subdir='test_data')
 
-esc50_csv = "../transferLearningYamnet/ESC-50/meta/esc50.csv"
-base_data_path = "../transferLearningYamnet/ESC-50/audio/"
+print(testing_wav_file_name)
+
+testing_wav_data = load_wav_16k_mono(testing_wav_file_name)
+
+
+esc50_csv = "../TransferLearningYamnet/ESC-50/meta/esc50.csv"
+base_data_path = "../TransferLearningYamnet/ESC-50/audio/"
 pd_data = pd.read_csv(esc50_csv)
 #print(pd_data)
 my_classes = ['dog', 'cat']
@@ -67,3 +77,59 @@ def extract_embedding(wav_data, label, fold):
 
 main_ds = main_ds.map(extract_embedding).unbatch()
 
+
+
+cached_ds = main_ds.cache()
+train_ds = cached_ds.filter(lambda embedding, label, fold: fold < 4)
+val_ds = cached_ds.filter(lambda embedding, label, fold: fold == 4)
+test_ds = cached_ds.filter(lambda embedding, label, fold: fold == 5)
+
+# remove the folds column now that it's not needed anymore
+remove_fold_column = lambda embedding, label, fold: (embedding, label)
+
+train_ds = train_ds.map(remove_fold_column)
+val_ds = val_ds.map(remove_fold_column)
+test_ds = test_ds.map(remove_fold_column)
+
+train_ds = train_ds.cache().shuffle(1000).batch(32).prefetch(tf.data.AUTOTUNE)
+val_ds = val_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+test_ds = test_ds.cache().batch(32).prefetch(tf.data.AUTOTUNE)
+
+
+my_model = tf.keras.Sequential([
+    tf.keras.layers.Input(shape=(1024,), dtype=tf.float32,
+                          name='input_embedding'),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(len(my_classes))
+], name='my_model')
+
+my_model.summary()
+
+
+my_model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                 optimizer="adam",
+                 metrics=['accuracy'])
+
+callback = tf.keras.callbacks.EarlyStopping(monitor='loss',
+                                            patience=3,
+                                            restore_best_weights=True)
+
+
+history = my_model.fit(train_ds,
+                       epochs=20,
+                       validation_data=val_ds,
+                       callbacks=callback)
+
+
+loss, accuracy = my_model.evaluate(test_ds)
+
+print("Loss: ", loss)
+print("Accuracy: ", accuracy)
+
+
+
+scores, embeddings, spectrogram = yamnet_model(testing_wav_data)
+result = my_model(embeddings).numpy()
+
+inferred_class = my_classes[result.mean(axis=0).argmax()]
+print(f'The main sound is: {inferred_class}')
